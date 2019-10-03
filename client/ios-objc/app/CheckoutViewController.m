cfeaf88465003eb034b78cb9aa6a6a0b5dabaaf3
Yuki Tokuhiro <yuki@stripe.com>
2019-10-03 09:39:30 -0700

    Objc

--- client/ios-objc/app/CheckoutViewController.m
+++ client/ios-objc/app/CheckoutViewController.m
@@ -20,9 +20,10 @@ NSString *const BackendUrl = @"http://127.0.0.1:4242/";
 
 @interface CheckoutViewController ()  <STPAuthenticationContext>
 
-@property (weak) STPPaymentCardTextField *cardTextField;
-@property (weak) UIButton *payButton;
-@property (strong) NSString *paymentIntentClientSecret;
+@property (nonatomic, weak) STPPaymentCardTextField *cardTextField;
+@property (nonatomic, weak) UIButton *payButton;
+@property (nonatomic, weak) UITextField *emailTextField;
+@property (nonatomic, copy) NSString *setupIntentClientSecret;
 
 @end
 
@@ -31,6 +32,11 @@ NSString *const BackendUrl = @"http://127.0.0.1:4242/";
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor whiteColor];
+    
+    UITextField *emailTextField = [UITextField new];
+    emailTextField.borderStyle = UITextBorderStyleRoundedRect;
+    emailTextField.placeholder = @"Enter your email";
+    self.emailTextField = emailTextField;
 
     STPPaymentCardTextField *cardTextField = [[STPPaymentCardTextField alloc] init];
     self.cardTextField = cardTextField;
@@ -39,11 +45,11 @@ NSString *const BackendUrl = @"http://127.0.0.1:4242/";
     button.layer.cornerRadius = 5;
     button.backgroundColor = [UIColor systemBlueColor];
     button.titleLabel.font = [UIFont systemFontOfSize:22];
-    [button setTitle:@"Pay" forState:UIControlStateNormal];
+    [button setTitle:@"Save" forState:UIControlStateNormal];
     [button addTarget:self action:@selector(pay) forControlEvents:UIControlEventTouchUpInside];
     self.payButton = button;
 
-    UIStackView *stackView = [[UIStackView alloc] initWithArrangedSubviews:@[cardTextField, button]];
+    UIStackView *stackView = [[UIStackView alloc] initWithArrangedSubviews:@[emailTextField, cardTextField, button]];
     stackView.axis = UILayoutConstraintAxisVertical;
     stackView.translatesAutoresizingMaskIntoConstraints = NO;
     stackView.spacing = 20;
@@ -55,28 +61,20 @@ NSString *const BackendUrl = @"http://127.0.0.1:4242/";
         [stackView.topAnchor constraintEqualToSystemSpacingBelowAnchor:self.view.topAnchor multiplier:2],
     ]];
 
-    [self loadPage];
+    [self startCheckout];
 }
 
-- (void)loadPage {
-    // Create a PaymentIntent by calling the sample server's /create-payment-intent endpoint.
-    NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"%@create-payment-intent", BackendUrl]];
-    NSDictionary *json = @{
-        @"currency": @"usd",
-        @"items": @[
-                @{@"id": @"photo_subscription"}
-        ]
-    };
-    NSData *body = [NSJSONSerialization dataWithJSONObject:json options:0 error:nil];
+- (void)startCheckout {
+    // Create a SetupIntent by calling the sample server's /create-setup-intent endpoint.
+    NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"%@create-setup-intent", BackendUrl]];
     NSMutableURLRequest *request = [[NSURLRequest requestWithURL:url] mutableCopy];
     [request setHTTPMethod:@"POST"];
     [request setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
-    [request setHTTPBody:body];
     NSURLSessionTask *task = [[NSURLSession sharedSession] dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *requestError) {
         NSError *error = requestError;
         NSDictionary *json = [NSJSONSerialization JSONObjectWithData:data options:0 error:&error];
         NSHTTPURLResponse *httpResponse = (NSHTTPURLResponse *)response;
-        if (error != nil || httpResponse.statusCode != 200 || json[@"publicKey"] == nil) {
+        if (error != nil || httpResponse.statusCode != 200 || json[@"publishableKey"] == nil) {
             dispatch_async(dispatch_get_main_queue(), ^{
                 NSString *message = error.localizedDescription ?: @"";
                 UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Error loading page" message:message preferredStyle:UIAlertControllerStyleAlert];
@@ -85,51 +83,59 @@ NSString *const BackendUrl = @"http://127.0.0.1:4242/";
             });
         }
         else {
-            NSLog(@"Created PaymentIntent");
-            self.paymentIntentClientSecret = json[@"clientSecret"];
-            NSString *stripePublicKey = json[@"publicKey"];
-            [Stripe setDefaultPublishableKey:stripePublicKey];
+            NSLog(@"Created Setupintent");
+            self.setupIntentClientSecret = json[@"clientSecret"];
+            NSString *stripePublishableKey = json[@"publishableKey"];
+            [Stripe setDefaultPublishableKey:stripePublishableKey];
         }
     }];
     [task resume];
 }
 
 - (void)pay {
-    if (!self.paymentIntentClientSecret) {
-        NSLog(@"PaymentIntent hasn't been created");
+    if (!self.setupIntentClientSecret) {
+        NSLog(@"SetupIntent hasn't been created");
         return;
     }
 
     // Collect card details
     STPPaymentMethodCardParams *cardParams = self.cardTextField.cardParams;
-    STPPaymentMethodParams *paymentMethodParams = [STPPaymentMethodParams paramsWithCard:cardParams billingDetails:nil metadata:nil];
-    STPPaymentIntentParams *paymentIntentParams = [[STPPaymentIntentParams alloc] initWithClientSecret:self.paymentIntentClientSecret];
-    paymentIntentParams.paymentMethodParams = paymentMethodParams;
+    
+    // Later, you will need to attach the PaymentMethod to the Customer it belongs to.
+    // This example collects the customer's email to know which customer the PaymentMethod belongs to, but your app might use an account id, session cookie, etc.
+    STPPaymentMethodBillingDetails *billingDetails = [STPPaymentMethodBillingDetails new];
+    billingDetails.email = self.emailTextField.text;
+    
+    // Create SetupIntent confirm parameters with the above
+    STPPaymentMethodParams *paymentMethodParams = [STPPaymentMethodParams paramsWithCard:cardParams billingDetails:billingDetails metadata:nil];
+    STPSetupIntentConfirmParams *setupIntentParams = [[STPSetupIntentConfirmParams alloc] initWithClientSecret:self.setupIntentClientSecret];
+    setupIntentParams.paymentMethodParams = paymentMethodParams;
 
-    // Submit the payment
+    // Complete the setup
     STPPaymentHandler *paymentHandler = [STPPaymentHandler sharedHandler];
-    [paymentHandler confirmPayment:paymentIntentParams withAuthenticationContext:self completion:^(STPPaymentHandlerActionStatus status, STPPaymentIntent *paymentIntent, NSError *error) {
+    [paymentHandler confirmSetupIntent:setupIntentParams withAuthenticationContext:self completion:^(STPPaymentHandlerActionStatus status, STPSetupIntent *setupIntent, NSError *error) {
         dispatch_async(dispatch_get_main_queue(), ^{
             switch (status) {
                 case STPPaymentHandlerActionStatusFailed: {
                     NSString *message = error.localizedDescription ?: @"";
-                    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Payment failed" message:message preferredStyle:UIAlertControllerStyleAlert];
+                    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Setup failed" message:message preferredStyle:UIAlertControllerStyleAlert];
                     [alert addAction:[UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleCancel handler:nil]];
                     [self presentViewController:alert animated:YES completion:nil];
                     break;
                 }
                 case STPPaymentHandlerActionStatusCanceled: {
                     NSString *message = error.localizedDescription ?: @"";
-                    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Payment canceled" message:message preferredStyle:UIAlertControllerStyleAlert];
+                    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Setup canceled" message:message preferredStyle:UIAlertControllerStyleAlert];
                     [alert addAction:[UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleCancel handler:nil]];
                     [self presentViewController:alert animated:YES completion:nil];
                     break;
                 }
                 case STPPaymentHandlerActionStatusSucceeded: {
-                    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Payment succeeded" message:paymentIntent.description preferredStyle:UIAlertControllerStyleAlert];
+                    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Setup succeeded" message:setupIntent.description preferredStyle:UIAlertControllerStyleAlert];
                     [alert addAction:[UIAlertAction actionWithTitle:@"Restart demo" style:UIAlertActionStyleCancel handler:^(UIAlertAction *action) {
                         [self.cardTextField clear];
-                        [self loadPage];
+                        self.emailTextField.text = nil;
+                        [self startCheckout];
                     }]];
                     [self presentViewController:alert animated:YES completion:nil];
                     break;
